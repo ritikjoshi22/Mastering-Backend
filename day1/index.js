968dfba878c4f5491178b6d0302c318c906355bf
ritikjoshi22 <joshster.ritik@gmail.com>
2025-06-29 18:33:11 +0545

    npm basics and advanced features

---
+++ day1/index.js
@@ -0,0 +1,20 @@
+// NPM underdstanding
+//     npm - Node Package Manager
+//     npm is a package manager for JavaScript, primarily used with Node.js
+//     It allows developers to install, share, and manage packages (libraries or tools) that can be used in their projects.
+
+// installing and uninstalling anything basics  & advanced
+//     npm init - initializes a new Node.js project and creates a package.json file
+//     npm install - installs a package and its dependencies
+//     npm uninstall - removes a package from the project
+
+//understanding node_modules
+//     node_modules - a directory where npm installs packages and their dependencies
+//     It contains all the packages that your project depends on, including their own dependencies.
+//dependencies and devDependencies
+//     dependencies - packages required for the application to run in production
+//     devDependencies - packages required only for development purposes, such as testing or build tools
+//scripts - understanding default scripts PATH and custom scripts
+//     scripts - commands defined in the package.json file that can be run using npm
+//     default scripts - npm provides some default scripts like start, test, and build
+//     custom scripts - you can define your own scripts in the package.json file to automate tasks
